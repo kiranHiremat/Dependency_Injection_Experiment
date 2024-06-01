99f1a7ccc2eb5e2c0592f08749abaed799c85340
kiran <www.kiran8861013490@gmail,com>
2024-06-02 00:45:58 +0530

    Practice Purpose

---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,16 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic : IDemoLogic
+{
+    private readonly ILogger<DemoLogic> _logger;
+
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+    public DemoLogic(ILogger<DemoLogic> logger)
+    {
+        Value1 = Random.Shared.Next(1, 1001);
+        Value2 = Random.Shared.Next(1, 1001);
+        _logger = logger;
+    }
+}
